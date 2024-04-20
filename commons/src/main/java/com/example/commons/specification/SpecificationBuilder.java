fd5620f8d12ebd95108ffbace277a3d467ec4e55
krlld <kirilldikun25@gmail.com>
2024-04-20 15:13:15 +0300

    refactor: allow filter dto to be null

--- commons/src/main/java/com/example/commons/specification/SpecificationBuilder.java
+++ commons/src/main/java/com/example/commons/specification/SpecificationBuilder.java
@@ -2,8 +2,9 @@ package com.example.commons.specification;
 
 import com.example.commons.dto.FilterRequest;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.lang.Nullable;
 
 public interface SpecificationBuilder<E> {
 
-    Specification<E> build(FilterRequest filterRequest);
+    Specification<E> build(@Nullable FilterRequest filterRequest);
 }
--- web/src/main/java/com/example/web/spec/NotificationSpecificationBuilder.java
+++ web/src/main/java/com/example/web/spec/NotificationSpecificationBuilder.java
@@ -6,17 +6,27 @@ import com.example.web.dto.NotificationFilterDto;
 import com.example.web.entity.Notification;
 import javax.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class NotificationSpecificationBuilder implements SpecificationBuilder<Notification> {
 
     @Override
-    public Specification<Notification> build(FilterRequest filterRequest) {
+    public Specification<Notification> build(@Nullable FilterRequest filterRequest) {
+        if (filterRequest == null) {
+            return (root, query, builder) -> builder.and();
+        }
         NotificationFilterDto notificationFilterDto = (NotificationFilterDto) filterRequest;
         return (root, query, builder) -> {
-            Predicate predicate = builder.equal(root.get("isRead"), notificationFilterDto.isRead());
-            return builder.and(predicate);
+            List<Predicate> predicates = new ArrayList<>();
+            if (notificationFilterDto.getIsRead() != null) {
+                predicates.add(builder.equal(root.get("isRead"), notificationFilterDto.getIsRead()));
+            }
+            return builder.and(predicates.toArray(new Predicate[0]));
         };
     }
 }
